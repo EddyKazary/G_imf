324bce445506176b57b41fc63e6756310031fbfd
EddyKazary <eddykazary7@gmail.com>
2022-09-20 18:42:20 +0200

    Premier commit

---
+++ src/main/java/com/gimf/efk/GImfApplication.java
@@ -0,0 +1,13 @@
+package com.gimf.efk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GImfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GImfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gimf/efk/GImfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimf.efk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GImfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
